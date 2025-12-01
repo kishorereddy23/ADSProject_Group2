0ef4827a1732cb5c2d24b68e6b0e3210c465b471
Kishore Reddy <venkatakishorepakala@gmail.com>
2025-12-01 09:17:57 +0100

    Completed all warm-up tasks: Half Adder, Full Adder, 4-Bit Adder, and Serial Receiver with comprehensive tests

--- 01_warm-up/src/main/scala/ReadSerial.scala
+++ 01_warm-up/src/main/scala/ReadSerial.scala
@@ -14,21 +14,42 @@ import chisel3.util._
 class Controller extends Module{
   
   val io = IO(new Bundle {
-    /* 
-     * TODO: Define IO ports of a the component as stated in the documentation
-     */
-    })
+    val rxd    = Input(Bool())      // Serial input line
+    val enable = Output(Bool())     // Enable signal for counter and shift register
+    val count  = Input(UInt(4.W))   // Counter value from counter
+    val valid  = Output(Bool())     // Valid output signal
+  })
 
-  // internal variables
-  /* 
-   * TODO: Define internal variables (registers and/or wires), if needed
-   */
+  // Internal state register
+  // States: 0 = IDLE, 1 = RECEIVING
+  val state = RegInit(0.U(1.W))
+  
+  // Register for valid signal to hold it for one cycle
+  val validReg = RegInit(false.B)
 
-  // state machine
-  /* 
-   * TODO: Describe functionality if the controller as a state machine
-   */
+  // Default outputs
+  io.enable := false.B
+  io.valid  := validReg
 
+  // State machine
+  when(state === 0.U) {
+    // IDLE state - waiting for start bit (0)
+    validReg := false.B
+    when(io.rxd === false.B) {
+      // Start bit detected, move to RECEIVING state
+      state := 1.U
+    }
+  } .otherwise {
+    // RECEIVING state - receiving data bits
+    io.enable := true.B
+    validReg := false.B  // Default to false
+    
+    when(io.count === 7.U) {
+      // All 8 bits received (counter at 7 means 8th bit is being sampled)
+      state := 0.U       // Go back to IDLE next cycle
+      validReg := true.B // Signal that data is valid
+    }
+  }
 }
 
 
@@ -36,77 +57,88 @@ class Controller extends Module{
 class Counter extends Module{
   
   val io = IO(new Bundle {
-    /* 
-     * TODO: Define IO ports of a the component as stated in the documentation
-     */
-    })
+    val enable = Input(Bool())     // Enable counting
+    val count  = Output(UInt(4.W)) // Current count value (0-7)
+  })
 
-  // internal variables
-  /* 
-   * TODO: Define internal variables (registers and/or wires), if needed
-   */
+  // Internal counter register (counts 0 to 7, then wraps)
+  val count = RegInit(0.U(4.W))
 
-  // state machine
-  /* 
-   * TODO: Describe functionality if the counter as a state machine
-   */
-
-
+  when(io.enable) {
+    when(count === 7.U) {
+      // Reached 7, reset to 0 on next enable
+      count := 0.U
+    } .otherwise {
+      // Increment counter
+      count := count + 1.U
+    }
+  } .otherwise {
+    // Not enabled, reset counter
+    count := 0.U
+  }
+  
+  // Output current count
+  io.count := count
 }
+
 
 /** shift register class */
 class ShiftRegister extends Module{
   
   val io = IO(new Bundle {
-    /* 
-     * TODO: Define IO ports of a the component as stated in the documentation
-     */
-    })
+    val rxd    = Input(Bool())       // Serial input
+    val enable = Input(Bool())       // Enable shifting
+    val data   = Output(UInt(8.W))   // Parallel output (8 bits)
+  })
 
-  // internal variables
-  /* 
-   * TODO: Define internal variables (registers and/or wires), if needed
-   */
+  // Internal 8-bit shift register
+  val shiftReg = RegInit(0.U(8.W))
 
-  // functionality
-  /* 
-   * TODO: Describe functionality if the shift register
-   */
+  when(io.enable) {
+    // MSB comes first, so we shift left and insert at LSB
+    shiftReg := Cat(shiftReg(6, 0), io.rxd)
+  }
+
+  io.data := shiftReg
 }
+
 
 /** 
   * The last warm-up task deals with a more complex component. Your goal is to design a serial receiver.
-  * It scans an input line (“serial bus”) named rxd for serial transmissions of data bytes. A transmission 
-  * begins with a start bit ‘0’ followed by 8 data bits. The most significant bit (MSB) is transmitted first. 
+  * It scans an input line ("serial bus") named rxd for serial transmissions of data bytes. A transmission 
+  * begins with a start bit '0' followed by 8 data bits. The most significant bit (MSB) is transmitted first. 
   * There is no parity bit and no stop bit. After the last data bit has been transferred a new transmission 
-  * (beginning with a start bit, ‘0’) may immediately follow. If there is no new transmission the bus line 
-  * goes high (‘1’, this is considered the “idle” bus signal). In this case the receiver waits until the next 
+  * (beginning with a start bit, '0') may immediately follow. If there is no new transmission the bus line 
+  * goes high ('1', this is considered the "idle" bus signal). In this case the receiver waits until the next 
   * transmission begins. The outputs of the design are an 8-bit parallel data signal and a valid signal. 
-  * The valid signal goes high (‘1’) for one clock cycle after the last serial bit has been transmitted, 
+  * The valid signal goes high ('1') for one clock cycle after the last serial bit has been transmitted, 
   * indicating that a new data byte is ready.
   */
 class ReadSerial extends Module{
   
   val io = IO(new Bundle {
-    /* 
-     * TODO: Define IO ports of a the component as stated in the documentation
-     */
-    })
+    val rxd   = Input(Bool())       // Serial input line
+    val data  = Output(UInt(8.W))   // Parallel data output
+    val valid = Output(Bool())      // Valid signal
+  })
 
+  // Instantiate modules
+  val controller = Module(new Controller())
+  val counter    = Module(new Counter())
+  val shiftReg   = Module(new ShiftRegister())
 
-  // instanciation of modules
-  /* 
-   * TODO: Instanciate the modules that you need
-   */
+  // Connect controller
+  controller.io.rxd   := io.rxd
+  controller.io.count := counter.io.count
 
-  // connections between modules
-  /* 
-   * TODO: connect the signals between the modules
-   */
+  // Connect counter
+  counter.io.enable := controller.io.enable
 
-  // global I/O 
-  /* 
-   * TODO: Describe output behaviour based on the input values and the internal signals
-   */
+  // Connect shift register  
+  shiftReg.io.rxd    := io.rxd
+  shiftReg.io.enable := controller.io.enable
 
+  // Global outputs
+  io.data  := shiftReg.io.data
+  io.valid := controller.io.valid
 }
--- 01_warm-up/src/test/scala/ReadSerialTester.scala
+++ 01_warm-up/src/test/scala/ReadSerialTester.scala
@@ -12,21 +12,181 @@ import org.scalatest.flatspec.AnyFlatSpec
 
 
 /** 
-  *read serial tester
+  * Read serial tester
+  * Tests the serial receiver with various data patterns
   */
 class ReadSerialTester extends AnyFlatSpec with ChiselScalatestTester {
 
   "ReadSerial" should "work" in {
     test(new ReadSerial).withAnnotations(Seq(WriteVcdAnnotation)) { dut =>
 
-        /*dut.io.rxd.poke(...)
-         *dut.clock.step(...)
-         *dut.io.valid.expect(...)
-         *dut.io.data.expect("b11111111".U) 
-         *...
-         *TODO: Add your testcases here
-         */
+        // Test 1: Transmit 0xFF (11111111)
+        println("Test 1: Sending 0xFF")
+        
+        // Initial state: bus is idle (high)
+        dut.io.rxd.poke(true.B)
+        dut.io.valid.expect(false.B)
+        dut.clock.step(1)
+        
+        // Start bit: 0
+        dut.io.rxd.poke(false.B)
+        dut.clock.step(1)
+        
+        // Data bits for 0xFF: 11111111 (MSB first)
+        // Bit 7
+        dut.io.rxd.poke(true.B)
+        dut.clock.step(1)
+        // Bit 6
+        dut.io.rxd.poke(true.B)
+        dut.clock.step(1)
+        // Bit 5
+        dut.io.rxd.poke(true.B)
+        dut.clock.step(1)
+        // Bit 4
+        dut.io.rxd.poke(true.B)
+        dut.clock.step(1)
+        // Bit 3
+        dut.io.rxd.poke(true.B)
+        dut.clock.step(1)
+        // Bit 2
+        dut.io.rxd.poke(true.B)
+        dut.clock.step(1)
+        // Bit 1
+        dut.io.rxd.poke(true.B)
+        dut.clock.step(1)
+        // Bit 0 (last bit)
+        dut.io.rxd.poke(true.B)
+        dut.clock.step(1)
+        
+        // NOW check - valid should be high after last bit is clocked in
+        dut.io.valid.expect(true.B)
+        dut.io.data.expect(0xFF.U)
+        
+        // Go back to idle
+        dut.io.rxd.poke(true.B)
+        dut.clock.step(1)
+        dut.io.valid.expect(false.B)
+        
+        
+        // Test 2: Transmit 0xA5 (10100101)
+        println("Test 2: Sending 0xA5")
+        dut.io.rxd.poke(true.B)
+        dut.clock.step(1)
+        
+        // Start bit
+        dut.io.rxd.poke(false.B)
+        dut.clock.step(1)
+        
+        // 0xA5 = 10100101 (MSB first)
+        dut.io.rxd.poke(true.B)   // Bit 7 = 1
+        dut.clock.step(1)
+        dut.io.rxd.poke(false.B)  // Bit 6 = 0
+        dut.clock.step(1)
+        dut.io.rxd.poke(true.B)   // Bit 5 = 1
+        dut.clock.step(1)
+        dut.io.rxd.poke(false.B)  // Bit 4 = 0
+        dut.clock.step(1)
+        dut.io.rxd.poke(false.B)  // Bit 3 = 0
+        dut.clock.step(1)
+        dut.io.rxd.poke(true.B)   // Bit 2 = 1
+        dut.clock.step(1)
+        dut.io.rxd.poke(false.B)  // Bit 1 = 0
+        dut.clock.step(1)
+        dut.io.rxd.poke(true.B)   // Bit 0 = 1
+        dut.clock.step(1)
+        
+        // Check output
+        dut.io.valid.expect(true.B)
+        dut.io.data.expect(0xA5.U)
+        
+        // Return to idle
+        dut.io.rxd.poke(true.B)
+        dut.clock.step(1)
+        dut.io.valid.expect(false.B)
+        
+        
+        // Test 3: Transmit 0x00 (00000000)
+        println("Test 3: Sending 0x00")
+        dut.io.rxd.poke(true.B)
+        dut.clock.step(1)
+        
+        // Start bit
+        dut.io.rxd.poke(false.B)
+        dut.clock.step(1)
+        
+        // 0x00 = 00000000
+        for(i <- 0 until 8) {
+          dut.io.rxd.poke(false.B)
+          dut.clock.step(1)
         }
-    } 
+        
+        // Check output
+        dut.io.valid.expect(true.B)
+        dut.io.data.expect(0x00.U)
+        
+        // Return to idle
+        dut.io.rxd.poke(true.B)
+        dut.clock.step(1)
+        dut.io.valid.expect(false.B)
+        
+        
+        // Test 4: Back-to-back transmissions
+        println("Test 4: Back-to-back 0x12 and 0x34")
+        dut.io.rxd.poke(true.B)
+        dut.clock.step(1)
+        
+        // First byte: 0x12 (00010010)
+        dut.io.rxd.poke(false.B)  // Start bit
+        dut.clock.step(1)
+        
+        // 0x12 = 00010010
+        dut.io.rxd.poke(false.B)  // Bit 7 = 0
+        dut.clock.step(1)
+        dut.io.rxd.poke(false.B)  // Bit 6 = 0
+        dut.clock.step(1)
+        dut.io.rxd.poke(false.B)  // Bit 5 = 0
+        dut.clock.step(1)
+        dut.io.rxd.poke(true.B)   // Bit 4 = 1
+        dut.clock.step(1)
+        dut.io.rxd.poke(false.B)  // Bit 3 = 0
+        dut.clock.step(1)
+        dut.io.rxd.poke(false.B)  // Bit 2 = 0
+        dut.clock.step(1)
+        dut.io.rxd.poke(true.B)   // Bit 1 = 1
+        dut.clock.step(1)
+        dut.io.rxd.poke(false.B)  // Bit 0 = 0
+        dut.clock.step(1)
+        
+        dut.io.valid.expect(true.B)
+        dut.io.data.expect(0x12.U)
+        
+        // Immediately start second byte: 0x34 (no idle)
+        dut.io.rxd.poke(false.B)  // Start bit
+        dut.clock.step(1)
+        dut.io.valid.expect(false.B)  // Valid should be low now
+        
+        // 0x34 = 00110100
+        dut.io.rxd.poke(false.B)  // Bit 7 = 0
+        dut.clock.step(1)
+        dut.io.rxd.poke(false.B)  // Bit 6 = 0
+        dut.clock.step(1)
+        dut.io.rxd.poke(true.B)   // Bit 5 = 1
+        dut.clock.step(1)
+        dut.io.rxd.poke(true.B)   // Bit 4 = 1
+        dut.clock.step(1)
+        dut.io.rxd.poke(false.B)  // Bit 3 = 0
+        dut.clock.step(1)
+        dut.io.rxd.poke(true.B)   // Bit 2 = 1
+        dut.clock.step(1)
+        dut.io.rxd.poke(false.B)  // Bit 1 = 0
+        dut.clock.step(1)
+        dut.io.rxd.poke(false.B)  // Bit 0 = 0
+        dut.clock.step(1)
+        
+        dut.io.valid.expect(true.B)
+        dut.io.data.expect(0x34.U)
+        
+        println("All Serial Receiver tests passed!")
+    }
+  } 
 }
-
